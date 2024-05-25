1adb632d84c66407357d787f70ef69c34d09ddd4
kottofey (KotBookPro) <romeokontakt@gmail.com>
2024-05-25 03:21:20 +0300

    sync commit

---
+++ src/scripts/modalHandlers.js
@@ -0,0 +1,20 @@
+// const burgerBtn = document.querySelector('.menu__header-item--button-burger');
+// const modal = document.querySelector('.article__content');
+//
+// const createMoreBtnHandler = function (button) {
+// 	button.addEventListener('click', function () {
+// 		let isExpanded = moreBtn.classList.contains('article__content--read-less');
+//
+// 		modal.style.height = `${modal.scrollHeight}px`;
+// 		moreBtn.textContent = 'Свернуть';
+// 		moreBtn.classList.toggle('article__content--read-less');
+//
+// 		if (isExpanded) {
+// 			modal.style.height = '200px';
+// 			moreBtn.textContent = 'Читать далее';
+// 		}
+// 		console.log('expanded:' + isExpanded);
+// 	});
+// };
+//
+// createMoreBtnHandler(burgerBtn);
