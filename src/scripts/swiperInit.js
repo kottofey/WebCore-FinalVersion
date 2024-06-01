d3c73263fa311ddce0ca47cbf644383a215cc7bd
kottofey (KotBookPro) <romeokontakt@gmail.com>
2024-06-01 03:52:43 +0300

    Markup finished, refactoring etc
    - Markup is done till the end (remaining some styles to fix)
    - Swiper markup structure universalized
    - Modal menu tag renamed to aside
    -  Styles to new elements
    - Color vars renamed to make them consistent
    - Aside menu height fix, on desktop width is full page height now but its content height limited to correspond figma layout

--- src/scripts/swiperInit.js
+++ src/scripts/swiperInit.js
@@ -1,7 +1,7 @@
 // html swiper init:
-// swiper global wrapper must have class '.repair-list'
-// swiper wrapper must have class '.repair-list__wrapper'
-// swiper card must have class '.repair-list__card'
+// swiper global wrapper must have class '.swiper'
+// swiper wrapper must have class '.swiper__wrapper'
+// swiper card must have class '.swiper__card'
 
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
@@ -10,13 +10,13 @@ import 'swiper/css/pagination';
 
 // Initialize swipers only for screens below 768px wide
 if (window.innerWidth < 768) {
-	let swipers = document.querySelectorAll('.repair-list');
-	let wrappers = document.querySelectorAll('.repair-list__wrapper');
+	let swipers = document.querySelectorAll('.swiper');
+	let wrappers = document.querySelectorAll('.swiper__wrapper');
 	let swiperPaginations = document.querySelectorAll('.swiper-pagination');
 
 	wrappers.forEach((wrapper) => {
-		wrapper.classList.add('swiper-wrapper');
-		wrapper.classList.remove('repair-list__wrapper');
+		wrapper.className = 'swiper-wrapper';
+		// wrapper.classList.remove('repair-list__wrapper');
 	});
 
 	swiperPaginations.forEach((pag) => {
@@ -26,14 +26,14 @@ if (window.innerWidth < 768) {
 	// init each found wrapper separately for calculating card width
 	// and proper scroll distance calculation
 	swipers.forEach((swiper) => {
-		let cardWidth = swiper.querySelector('.repair-list__card').scrollWidth;
+		let cardWidth = swiper.querySelector('.swiper__card').scrollWidth;
 
 		new Swiper(swiper, {
 			modules: [Pagination],
 			direction: 'horizontal',
 			slidesOffsetBefore: 16,
 			slidesOffsetAfter: 16,
-			slideClass: 'repair-list__card',
+			slideClass: 'swiper__card',
 			pagination: {
 				el: '.swiper-pagination',
 			},
