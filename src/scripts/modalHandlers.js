a96318db5e177aaa6478e31402d2d6644a18e923
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-06-07 01:54:36 +0300

    Refactring and JS
    Added interactive conternt to buttons for call and feedback and burger menu

--- src/scripts/modalHandlers.js
+++ src/scripts/modalHandlers.js
@@ -1,20 +1,62 @@
-// const burgerBtn = document.querySelector('.menu__header-item--button-burger');
-// const modal = document.querySelector('.article__content');
-//
-// const createMoreBtnHandler = function (button) {
-// 	button.addEventListener('click', function () {
-// 		let isExpanded = moreBtn.classList.contains('article__content--read-less');
-//
-// 		modal.style.height = `${modal.scrollHeight}px`;
-// 		moreBtn.textContent = 'Свернуть';
-// 		moreBtn.classList.toggle('article__content--read-less');
-//
-// 		if (isExpanded) {
-// 			modal.style.height = '200px';
-// 			moreBtn.textContent = 'Читать далее';
-// 		}
-// 		console.log('expanded:' + isExpanded);
-// 	});
-// };
-//
-// createMoreBtnHandler(burgerBtn);
+const modalMenus = document.querySelectorAll('.modal-menu');
+const modalBlur = document.querySelector('.modal-blur');
+const modalMenuDesktop = document.querySelector('.modal-menu--desktop');
+const btnBurger = document.querySelector('.menu__header-item--button-burger');
+const btnsCall = document.querySelectorAll('.menu__header-item--button-call');
+const btnsFeedback = document.querySelectorAll('.menu__header-item--button-feedback');
+
+function modalMenuOn(modalMenu) {
+	modalBlur.classList.add('modal-blur--on');
+	if (modalMenu.classList.contains('modal-extra')) {
+		modalMenu.classList.add('modal-extra--is-on');
+	} else {
+		modalMenu.classList.add('modal-menu--is-on');
+	}
+}
+
+function modalMenuOff(modalMenu) {
+	modalBlur.classList.remove('modal-blur--on');
+	modalMenu.classList.remove('modal-menu--is-on', 'modal-extra--is-on');
+}
+
+const createOpenBtnHandler = function (button, modalMenu) {
+	button.addEventListener('click', function () {
+		modalMenuOn(modalMenu);
+	});
+};
+
+const createCloseBtnHandler = function (modalMenu) {
+	const closeBtn = modalMenu.querySelector('.menu__header-item--button-close');
+
+	closeBtn.addEventListener('click', function () {
+		modalMenuOff(modalMenu);
+		console.log('btn close');
+	});
+	modalBlur.addEventListener('click', function () {
+		modalMenuOff(modalMenu);
+		console.log('blur off');
+	});
+	window.addEventListener('keydown', (evt) => {
+		if (evt.key === 'Escape') {
+			modalMenuOff(modalMenu);
+			console.log('esc off');
+		}
+	});
+};
+
+// creating handlers
+createOpenBtnHandler(btnBurger, modalMenuDesktop);
+
+modalMenus.forEach((modalMenu) => {
+	createCloseBtnHandler(modalMenu);
+});
+
+btnsFeedback.forEach((btnFeedback) => {
+	const modalFeedback = document.querySelector('.modal-extra--feedback');
+	createOpenBtnHandler(btnFeedback, modalFeedback);
+});
+
+btnsCall.forEach((btnCall) => {
+	const modalCall = document.querySelector('.modal-extra--call');
+	createOpenBtnHandler(btnCall, modalCall);
+});
